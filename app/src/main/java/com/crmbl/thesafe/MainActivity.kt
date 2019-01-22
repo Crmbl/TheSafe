be9cd17fe63a07e2069e969d9097220ef383428e
SCHAEFAX <axel.schaeffer@externe.e-i.com>
2019-01-22 17:04:01 +0100

    decrypt seems to work

--- app/src/main/java/com/crmbl/thesafe/MainActivity.kt
+++ app/src/main/java/com/crmbl/thesafe/MainActivity.kt
@@ -3,14 +3,12 @@ package com.crmbl.thesafe
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.util.Base64
-import android.util.Log
+import java.io.File
 import java.io.InputStream
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 import javax.crypto.spec.IvParameterSpec
-import kotlin.experimental.and
-
+import android.os.Environment
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,47 +16,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val userNameTxt = findViewById<EditText>(R.id.userNameTxt)
-//        val passwordTxt = findViewById<EditText>(R.id.passwordTxt)
-//        val resetBtn = findViewById<Button>(R.id.resetBtn)
-//        val submitBtn = findViewById<Button>(R.id.submitBtn)
-//
         val fingerButton = findViewById<FloatingActionButton>(R.id.fingerButton)
         fingerButton.setOnClickListener {
 
             var ins: InputStream = assets.open("docd.qsp")
-            var content : ByteArray = ins.readBytes()
+            var origin : ByteArray = ins.readBytes()
             ins.close()
 
-            var byteArray : Array<UByte> = Array(content.size) { i: Int -> content[i].toUByte() }
-            val key = "3BVEnYwzN8eNTG8G".toByteArray()
             val salt = "EsyQJ7keJK2nkVJ8".toByteArray()
+            val passPhrase = "3BVEnYwzN8eNTG8G"
+            var password = PasswordDeriveBytes(passPhrase, salt, "SHA1", 2)
+            var pass32 : ByteArray = password.GetBytes(32)
+            var pass16 : ByteArray = password.GetBytes(16)
+²
             try {
-                val iv = IvParameterSpec(salt)
-                val keySpec = SecretKeySpec(key, "SHA1PRNG")
-                val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
-                cipher.init(Cipher.DECRYPT_MODE, keySpec, iv)
-                val original = cipher.doFinal(Base64.decode(byteArray as ByteArray, byteArray.size))
+                val cipher = Cipher.getInstance("AES/CBC/NoPadding ")
+                cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(pass32, "SHA1PRNG"), IvParameterSpec(pass16))
+                val decrypted = cipher.doFinal(origin)
 
+                val outputFile = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "/decrypted.gif")
+                outputFile.writeBytes(decrypted)
+
+                val originFile = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "/crypted.gif")
+                originFile.writeBytes(origin)
             } catch (ex: Exception) {
                 ex.printStackTrace()
             }
         }
     }
-
-//    @Throws(Exception::class)
-//    private fun encrypt(raw: ByteArray, clear: ByteArray): ByteArray {
-//        val skeySpec = SecretKeySpec(raw, "AES/CBC")
-//        val cipher = Cipher.getInstance("AES/CBC")
-//        cipher.init(Cipher.ENCRYPT_MODE, skeySpec)
-//        return cipher.doFinal(clear)
-//    }
-//
-//    @Throws(Exception::class)
-//    private fun decrypt(raw: ByteArray, encrypted: ByteArray): ByteArray {
-//        val skeySpec = SecretKeySpec(raw, "AES/CBC")
-//        val cipher = Cipher.getInstance("AES/CBC")
-//        cipher.init(Cipher.DECRYPT_MODE, skeySpec)
-//        return cipher.doFinal(encrypted)
-//    }
 }
