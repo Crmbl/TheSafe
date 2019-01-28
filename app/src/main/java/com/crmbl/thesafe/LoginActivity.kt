abaab9bb8c11c0a97baa5b2f0b7aa689bad5183a
SCHAEFAX <axel.schaeffer@externe.e-i.com>
2019-01-28 16:32:33 +0100

    Final popup for text/fingerprint login

--- app/src/main/java/com/crmbl/thesafe/LoginActivity.kt
+++ app/src/main/java/com/crmbl/thesafe/LoginActivity.kt
@@ -3,9 +3,8 @@ package com.crmbl.thesafe
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
-import android.widget.LinearLayout
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.animation.AnimationUtils
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 
@@ -16,30 +15,42 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_login)
 
-//        var viewModel = LoginViewModel()
-//        viewModel.username = "Test"
-//        viewModel.password = "Plouf"
-
-        var loginButtonText = findViewById<MaterialButton>(R.id.login_button_text)
-        loginButtonText.setOnClickListener{this.showPasswordCard()}
-
         var loginButtonCancel = findViewById<MaterialButton>(R.id.login_button_cancel)
+        var loginButtonText = findViewById<MaterialButton>(R.id.login_button_text)
+        var loginButtonFinger = findViewById<MaterialButton>(R.id.login_button_finger)
+        var loginButtonCancelF = findViewById<MaterialButton>(R.id.login_button_cancel_fingerprint)
+
+        loginButtonText.setOnClickListener{this.showPasswordCard()}
         loginButtonCancel.setOnClickListener{this.hidePasswordCard()}
+        loginButtonFinger.setOnClickListener{this.showFingerprintCard()}
+        loginButtonCancelF.setOnClickListener{this.hideFingerprintCard()}
     }
 
     private fun showPasswordCard() {
-        var buttonsLayout = findViewById<LinearLayout>(R.id.buttons_layout)
         var loginCardPassword = findViewById<MaterialCardView>(R.id.login_card_password)
-        var buttonsLayoutPos = buttonsLayout.y + buttonsLayout.height
-        loginCardPassword.y = buttonsLayoutPos - loginCardPassword.height
-
-//        add slide up effect
-//        var slideUp : Animation = AnimationUtils.
+        var slideUp : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_up)
         loginCardPassword.visibility = View.VISIBLE
+        loginCardPassword.startAnimation(slideUp)
     }
 
     private fun hidePasswordCard() {
         var loginCardPassword = findViewById<MaterialCardView>(R.id.login_card_password)
+        var slideDown : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_down)
         loginCardPassword.visibility = View.INVISIBLE
+        loginCardPassword.startAnimation(slideDown)
+    }
+
+    private fun showFingerprintCard() {
+        var loginCardFingerprint = findViewById<MaterialCardView>(R.id.login_card_fingerprint)
+        var slideUp : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_up)
+        loginCardFingerprint.visibility = View.VISIBLE
+        loginCardFingerprint.startAnimation(slideUp)
+    }
+
+    private fun hideFingerprintCard() {
+        var loginCardFingerprint = findViewById<MaterialCardView>(R.id.login_card_fingerprint)
+        var slideDown : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_down)
+        loginCardFingerprint.visibility = View.INVISIBLE
+        loginCardFingerprint.startAnimation(slideDown)
     }
 }
