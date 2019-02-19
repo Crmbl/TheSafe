a30c1eb4d5cf7c66cabe024919c4696d40da44d6
SCHAEFAX <axel.schaeffer@externe.e-i.com>
2019-02-19 16:09:08 +0100

    GifViewer implementation

--- app/src/main/java/com/crmbl/thesafe/ItemAdapter.kt
+++ app/src/main/java/com/crmbl/thesafe/ItemAdapter.kt
@@ -1,65 +1,76 @@
 package com.crmbl.thesafe
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.*
+import pl.droidsonroids.gif.GifDrawable
+import pl.droidsonroids.gif.GifImageView
 
 
+//TODO Gif getting slower and slower... NOT GOOD ! Might need to change for RecycleView?
+//TODO Using convertView, needs to remove the marginTop in some way. Must find another way of doing it
 class ItemAdapter(_context : Context, private val dataSource : MutableList<File>) : BaseAdapter() {
     private val inflater : LayoutInflater = _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     private val context : Context = _context
 
-    @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val rowView = inflater.inflate(R.layout.list_item, parent, false)
+        val rowView = convertView ?: inflater.inflate(R.layout.list_item, parent, false)
         rowView.alpha = 0f
         val titleView = rowView.findViewById<TextView>(R.id.textview_title)
         val extensionView = rowView.findViewById<TextView>(R.id.textview_ext)
-        val imageView : ImageView = rowView.findViewById(R.id.imageView)
-
         val file = getItem(position) as File
         val splitedName = file.originName.split('.')
-        val bitMap = BitmapFactory.decodeByteArray(file.decrypted, 0, file.decrypted?.size!!)
 
+        val mediaView : View
+        if (splitedName[1].toLowerCase() == "gif" /*|| todo add other image ext */) {
+            val gifDrawable = GifDrawable(file.decrypted!!)
+            mediaView = rowView.findViewById<GifImageView>(R.id.imageView)
+            mediaView.setImageDrawable(gifDrawable)
+            gifDrawable.start()
+        } else {
+            mediaView = rowView.findViewById<VideoView>(R.id.videoView)
+//            (mediaView as VideoView).setV
+        }
+
+        mediaView.visibility = View.VISIBLE
         titleView.text = splitedName[0]
         extensionView.text = splitedName[1]
         file.position = position
-        imageView.setImageBitmap(bitMap)
+
+        //region height management
 
         if (file.height != 0) {
             if (position == 0) {
-                val params = imageView.layoutParams as RelativeLayout.LayoutParams
+                val params = mediaView.layoutParams as RelativeLayout.LayoutParams
                 val offset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 42f, context.resources.displayMetrics).toInt()
                 params.topMargin = offset
-                imageView.layoutParams = params
+                mediaView.layoutParams = params
             }
-            imageView.minimumHeight = file.height
+            mediaView.minimumHeight = file.height
             rowView.minimumHeight = file.totalHeight
         }
 
         rowView.viewTreeObserver.addOnPreDrawListener (object : ViewTreeObserver.OnPreDrawListener {
             override fun onPreDraw(): Boolean {
                 val linearLayout : LinearLayout = rowView.findViewById(R.id.bottom_layout)
-                if (rowView.minimumHeight == 0 && imageView.minimumHeight == 0 && file.height == 0) {
+                if (rowView.minimumHeight == 0 && mediaView.minimumHeight == 0 && file.height == 0) {
                     if (position == 0) {
-                        val params = imageView.layoutParams as RelativeLayout.LayoutParams
+                        val params = mediaView.layoutParams as RelativeLayout.LayoutParams
                         val offset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 42f, context.resources.displayMetrics).toInt()
                         params.topMargin = offset
-                        imageView.layoutParams = params
-                        imageView.minimumHeight = imageView.height + offset
-                        rowView.minimumHeight = imageView.height + linearLayout.height + offset
+                        mediaView.layoutParams = params
+                        mediaView.minimumHeight = mediaView.height + offset
+                        rowView.minimumHeight = mediaView.height + linearLayout.height + offset
                     } else {
-                        rowView.minimumHeight = imageView.height + linearLayout.height
-                        imageView.minimumHeight = imageView.height
+                        rowView.minimumHeight = mediaView.height + linearLayout.height
+                        mediaView.minimumHeight = mediaView.height
                     }
 
-                    file.height = imageView.minimumHeight
+                    file.height = mediaView.minimumHeight
                     file.totalHeight = rowView.minimumHeight
                 }
 
@@ -70,6 +81,9 @@ class ItemAdapter(_context : Context, private val dataSource : MutableList<File>
         rowView.animate().alpha(1f).setDuration(125).withEndAction {
             rowView.alpha = 1f
         }.start()
+
+        //endregion height management
+
         return rowView
     }
 
